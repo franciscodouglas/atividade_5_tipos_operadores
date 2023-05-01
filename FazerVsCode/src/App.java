f6b7b1f5871fefa2c8db811b331e64df8b3e1893
Francisco Douglas <98854868+franciscodouglas@users.noreply.github.com>
2023-05-01 10:58:41 -0300

    Add files via upload

---
+++ FazerVsCode/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+        //Comentário
+
+    public static void funcao_teste (){
+        //Biblioteca para entrada de dados
+        Scanner ler = new Scanner(System.in);
+
+        //variável local 
+        String v2;
+        int v1;
+
+        System.out.println("Comando de Saida de Dados");
+        
+        System.out.printf("Informe uma palavra: ");
+        v2 = ler.next();
+
+        System.out.printf("Informe um número: ");
+        v1 = ler.nextInt();
+
+        System.out.printf(v2+" "+v1);
+    }
+    public static void main(String[] args) throws Exception {
+       Corpo_Humano c1 = new Corpo_Humano(80, 0, 1.78);
+
+       System.out.println(c1.calcularIMC());
+
+
+    }
+}
